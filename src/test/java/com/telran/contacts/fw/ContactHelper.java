504ca0997028652b5392ffb37c1d1894243081b6
Elena Lebedeva <89071540+ElenaL171@users.noreply.github.com>
2022-07-12 16:34:05 +0200

    HA 12.07

---
+++ src/test/java/com/telran/contacts/fw/ContactHelper.java
@@ -0,0 +1,70 @@
+package com.telran.contacts.fw;
+
+import com.telran.contacts.models.Contact;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class ContactHelper extends HelperBase {
+    public ContactHelper(WebDriver driver) {
+        super(driver);
+    }
+
+    public boolean isComponentFormPresent() {
+        return driver.findElements(By.cssSelector("div:nth-child(2)>div>div")).size() > 0;
+    }
+
+
+    public boolean isContactCreated(String text) {
+        List<WebElement> contacts = driver.findElements(By.cssSelector("h2"));
+        for (WebElement el : contacts) {
+            if (el.getText().contains(text)) ;
+            return true;
+        }
+        return false;
+    }
+
+    public void addContakt() {
+        int i = (int) (System.currentTimeMillis()) / 10000000;
+        click(By.xpath("//a[contains(text(),'ADD')]"));
+        fillContactForm(new Contact()
+                .setName("Karl")
+                .setLastname("Kurz")
+                .setPhone("9875634" + i)
+                .setAddress("adam" + i + "@gmail.com")
+                .setCity("Hannover")
+                .setDescription("hiere"));
+        clickWithAction(By.cssSelector(".add_form__2rsm2 button"));
+    }
+
+    public void fillContactForm(Contact contact) {
+        type(By.cssSelector("input:nth-child(1)"), contact.getName());
+        type(By.cssSelector("input:nth-child(2)"), contact.getLastname());
+        type(By.cssSelector("input:nth-child(3)"), contact.getPhone());
+        type(By.cssSelector("input:nth-child(4)"), contact.getAddress());
+        type(By.cssSelector("input:nth-child(5)"), contact.getCity());
+        type(By.cssSelector("input:nth-child(6)"), contact.getDescription());
+    }
+
+    public int sizeOfContacts() {
+        if (isElementPresent(By.cssSelector(".contact-item_card__2SOIM"))) {
+            return driver.findElements(By.cssSelector(".contact-item_card__2SOIM")).size();
+        }
+        return 0;
+    }
+
+    public void removeContact() {
+        if (!isContactListEmpty()) {
+            click(By.cssSelector(".contact-item_card__2SOIM"));
+            click(By.xpath("//button[contains(.,'Remove')]"));
+            // //button[text()='Remove'
+        }
+
+    }
+
+    public boolean isContactListEmpty() {
+        return driver.findElements(By.cssSelector(".contact-item_card__2SOIM")).isEmpty();
+    }
+}
